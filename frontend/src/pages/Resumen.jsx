e501ca316137ae9247b406051238c99b572fca05
HectorUM <hector7x@hotmail.com>
2026-05-28 18:11:14 -0700

    Update Resumen.jsx

--- frontend/src/pages/Resumen.jsx
+++ frontend/src/pages/Resumen.jsx
@@ -249,8 +249,8 @@ export default function Resumen({ estructuraGeografica }) {
                                     <button onClick={()=>setModoEdicion(false)} className="bg-slate-700 p-1.5 rounded cursor-pointer"><X className="w-4 h-4" /></button>
                                 </div>
                             ) : (
-                                <div className="flex items-center gap-3 group">
-                                    <p className="text-2xl font-black justify-center text-blue-400 font-mono">{capacidadTotal}</p>
+                                <div className="flex items-center gap-3 justify-center group">
+                                    <p className="text-2xl font-black text-blue-400 font-mono">{capacidadTotal}</p>
                                     <button onClick={()=>{setEditCapacidad(capacidadTotal); setModoEdicion(true);}} className="text-slate-600 hover:text-blue-400 cursor-pointer"><Edit2 className="w-4 h-4" /></button>
                                 </div>
                             )}
