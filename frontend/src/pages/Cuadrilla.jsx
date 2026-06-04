955e09c1ec93e2ff5ab0224b897dd7555a92b49b
HectorUM <hector7x@hotmail.com>
2026-06-03 17:21:31 -0700

    Update Cuadrilla.jsx

--- frontend/src/pages/Cuadrilla.jsx
+++ frontend/src/pages/Cuadrilla.jsx
@@ -166,7 +166,7 @@ export default function Cuadrilla({ token }) {
 
               {/* Bloque: Ubicación y Contacto (Solo Lectura) */}
               <div className="bg-[#0b132b] border border-slate-800 rounded-xl p-4 shadow-sm">
-                <h3 className="text-pink-400 font-black text-[11px] uppercase tracking-widest mb-2 flex items-center gap-2 border-b border-slate-800 pb-2"><Users className="w-4 h-4"/> Contacto y Sitio</h3>
+                <h3 className="text-pink-400 font-black text-[11px] uppercase tracking-widest mb-2 flex items-center gap-2 border-b border-slate-400 pb-2"><Users className="w-4 h-4"/> Contacto y Sitio</h3>
                 <InfoRow label="Nombre Contacto" value={puertoActivo.CONTACTO_NOMBRE} />
                 <InfoRow label="Teléfono" value={puertoActivo.CONTACTO_TELEFONO} isPhone={true} />
                 
